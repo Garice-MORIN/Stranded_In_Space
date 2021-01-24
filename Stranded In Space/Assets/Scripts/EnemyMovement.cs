fd4e1f70246c270a616a6818538c98ffe89887ec
Garice MORIN <32975927+Porschefana@users.noreply.github.com>
2021-01-24 11:30:24 +0100

    Better movement + spawn enemies
    Ennemis spawnent, possibilité de courir ou marcher

---
+++ Stranded In Space/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Rigidbody enemy;
+    
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        enemy.AddForce(100*Time.deltaTime,0,100*Time.deltaTime);
+    }
+}
---
+++ Stranded In Space/Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform spawnPoint;
+    public int waveSize;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Enemies());
+    }
+
+    IEnumerator Enemies ()
+    {
+        while (waveSize > 0)
+        {
+            Vector3 pos = spawnPoint.position;
+            Instantiate(enemy, pos, Quaternion.identity);
+            yield return new WaitForSeconds (0.5f);
+            waveSize--;
+        }
+    }
+}

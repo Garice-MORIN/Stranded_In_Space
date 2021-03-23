da9afab23248396f8671a5f2df242c88fe55cabc
Garice-MORIN <76947301+Garice-MORIN@users.noreply.github.com>
2021-03-23 17:09:08 +0100

    Debut AI

---
+++ SIS-2.0/SIS-2.0/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent navMesh = GetComponent<NavMeshAgent>();
+        goal = GameObject.FindGameObjectWithTag("Goal").transform;
+        navMesh.destination = goal.position;
+    }
+}
